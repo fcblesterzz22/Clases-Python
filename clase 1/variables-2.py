b4cb23b9b74c124fad0d4c2202ae6a112e416ba9
fcblesterzz22 <lesterpinango.8@gmail.com>
2025-10-03 23:05:14 +0000

    CLASE 1 MANUAL Y VARIABLES

---
+++ clase 1/variables-2.py
@@ -0,0 +1,21 @@
+#variables
+a = 25
+b = 5
+
+#Proceso o la operacion realizada
+r = a+b
+
+#OPERACIONES BASICAS DE UNA CALCULADORA
+r = a+b
+d = a-b
+c = a*b 
+z = a/b
+
+#EL RESULTADO ESPERADO
+print(f"la suma de {a}+{b} es igual a {r} ")
+
+print(f"la resta de {a}-{b} es igual a {d}" )
+
+print(f"la multiplicacion de  {a}x{b} es igual a {c}" )
+
+print(f"la division de  {a} entre {b} es igual a {z}" )
---
+++ clase 1/variables.py
@@ -0,0 +1,13 @@
+# str (cadena de valor) ----> "TEXTO" 
+# int (entero) --> 10
+# float (números con decimales) --> 10.5
+# bool (booleano) --> True or False
+
+# Formato de las variables
+variable = "valor"
+##########################
+nombre = "Lester"
+edad = "13"
+ciudad = "Lima"
+
+print (f"mi nombre es {nombre}, tengo {edad} años y vivo en {ciudad}")
